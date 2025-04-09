d94ffb5735667fa0f81757dbcff2f1c08d09465b
Tim Macfarlane <timmacfarlane@gmail.com>
2025-04-09 11:58:02 +0200

    simpler nav and shift

--- keyboards/splitkb/aurora/sofle_v2/keymaps/tim/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/tim/keymap.c
@@ -41,7 +41,7 @@ uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB             , KC_Q               , KC_W               , KC_E               , KC_R               , KC_T               ,                                           KC_Y               , KC_U               , KC_I               , KC_O               , KC_P               , KC_BSPC            ,
         KC_ESC             , KC_A               , LTNUM(KC_S)        , LTSYM(KC_D)        , LTBRC(KC_F)        , KC_G               ,                                           KC_H               , KC_J               , KC_K               , KC_L               , KC_SCLN            , KC_QUOT            ,
         CW_TOGG            , KC_Z               , KC_X               , KC_C               , KC_V               , KC_B               , KC_MUTE            , KC_MPLY            , KC_N               , KC_M               , KC_COMM            , KC_DOT             , KC_SLSH            , KC_MINS            ,
-                                                  KC_LGUI            , KC_LCTL            , KC_LALT            , TD(TD_SHIFT_LAYER) , KC_ENT             , KC_SPC             , OSM(MOD_LSFT)      , KC_RALT            , KC_RCTL            , KC_RGUI
+                                                  KC_LGUI            , KC_LCTL            , KC_LALT            , MO(L_NAV)          , KC_ENT             , KC_SPC             , KC_LSFT            , KC_RALT            , KC_RCTL            , KC_RGUI
     ),
 
     [L_GRAP] = LAYOUT(
